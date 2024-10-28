8a0b5d2e83b22acb2b3e78d139aaa875e7d3bc74
AILISUSAN <33285547+ailisusan@users.noreply.github.com>
2024-10-28 20:54:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,58 @@
 <div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+<section id="intro" class="content-wrapper">
+	
+	<div class="article-container">
+
+		<article class="article">
+
+			<h2>Hjärtligt välkommen</h2>
+						
+			<p>
+			Vi är en församling belägen i centrala Nyköping vid Tingshusplatsen. Vi vill vara en levande församling som med hjälp av den Helige Andes kraft och kärlek får vara en plats för läkedom till hela människan. Vi vill att alla i Nyköpingsbygden (och utanför) ska känna Guds kärlek och tror att en väsentlig del av församlingens uppdrag är att reflektera det.</p>
+			
+			<p>Varmt välkommen till oss!</p>
+			
+			
+			<button class="transparent"><a href="/om">Vill du veta mer?</a></button>
+	
+		</article>
+		
+		<article class="article">
+ 
+			<h2>Vi ses i kyrkan!</h2>
+						
+			<p><b>Familjegrupp</b></br>Första tisdagen i månaden kl. 17:00</p>
+			<p><b>Bön i kyrkan</b></br>Varje torsdag kl. 10:00 och 19:00</p>
+			<p><b>Tweenies för mellanstadiebarn</b></br>Fredag jämna veckor kl. 18:00</p>
+			<p><b>Gudstjänst och gemenskap</b></br>Varje söndag kl. 15:00</p>
+			
+	
+			<button class="transparent"><a href="/kalender">Gå till kalender</a></button>
+  
+		</article>
+
+		<article class="article">
+
+			<h2>Behöver du prata med någon?</h2>
+	
+			<p>Du är välkommen att höra av dig till oss och boka en tid för ett själavårdande samtal, oavsett om du är medlem eller inte. Med själavård menar vi omsorg om människans existentiella, mentala och andliga hälsa.</p>
+			
+			<p>Vi tror att det själavårdande samtalet kan vara till hjälp även om man inte är troende. Samtalet kan ske i kyrkan, hemma hos dig eller hos själavårdaren, via telefon eller i under en promenad.</p>
+	
+
+			<button class="transparent"><a href="/om#sjalavard">Mer om själavård</a></button>
+
+		</article>
+		
+	</div>
+         
+	</section> <!--- Slut på introavsnitt -->
+
+		<section id="aktuellt" class="content-wrapper">
+		<h1>Aktuellt</h1>
+			
 		<?php
 		if ( have_posts() ) :
 
@@ -32,6 +84,24 @@
 			get_template_part( 'components/content', 'none' );
 
 		endif; ?>
+
+		</section>
+
+		<section class="kors" class="content-wrapper">
+	
+	<div class="content-container">
+	
+		<p>&#10013;</p>
+        
+	</div>
+	
+    </section>
+	
+	<section id="mellanbild-1" class="content-wrapper mellanbild">
+	
+	</section>
+
+		
 
 		</main>
 	</div>
