34b271b0a678887866b932819fba0a97ac88e19b
AILISUSAN <33285547+ailisusan@users.noreply.github.com>
2024-10-28 19:43:25 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php ?>
+
+	</div>
+	<footer id="colophon" class="site-footer" role="contentinfo">
+
+		<?php affinity_social_menu(); ?>
+
+		<?php get_template_part( 'components/footer/footer', 'widgets' ); ?>
+
+		<?php get_template_part( 'components/footer/site', 'info' ); ?>
+	</footer>
+</div>
+<?php wp_footer(); ?>
+
+</body>
+</html>
